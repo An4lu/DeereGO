f4d6ac436a68922a627bebafa55c525fdbc7cefb
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-19 11:17:23 -0300

    main + config react

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+export function Cards() {
+    return (
+        <div className="cards-container">
+            <div className="card">
+                <h3>Tempo Médio</h3>
+                <p>15m</p>
+            </div>
+            <div className="card">
+                <h3>Tempo Médio</h3>
+                <p>15m</p>
+            </div>
+            <div className="card">
+                <h3>Tempo Médio</h3>
+                <p>15m</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,26 @@
+import { Gear, House, Package, User } from "@phosphor-icons/react";
+import React from "react";
+import './styles.css';
+
+export function Menu() {
+    return (
+        <nav className="menu">
+            <div className="icone">
+                <House size={32}></House>
+                <a>Home</a>
+            </div>
+            <div className="icone">
+                <User size={32}></User>
+                <a>Perfil</a>
+            </div>
+            <div className="icone">
+                <Package size={32}></Package>
+                <a>Entregas</a>
+            </div>
+            <div className="icone">
+                <Gear size={32}></Gear>
+                <a>Ajustes</a>
+            </div>
+        </nav>
+    );
+}
