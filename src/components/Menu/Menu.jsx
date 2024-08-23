ee77b1f5dce7e8bf3a48dca88ae5e22afba5e56d
AnaluPc <anabluoliveira@outlook.com>
2024-08-23 10:59:11 -0300

    Alteração do Menu e Sizes

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,15 +7,15 @@ export function Menu() {
     return (
         <nav className="menu">
             <div className="icone-active">
-                <House size={32}></House>
+                <House size={38} weight="fill"></House>
                 <Link to={`/`}>Home</Link>
             </div>
             <div className="icone">
-                <User size={32}></User>
+                <User size={38} weight="fill" ></User>
                 <Link to={`/perfil`}>Perfil</Link>
             </div>
             <div className="icone">
-                <Package size={32}></Package>
+                <Package size={38} weight="fill"></Package>
                 <Link to={`/entregas`}>Entregas</Link>
             </div>
         </nav>
