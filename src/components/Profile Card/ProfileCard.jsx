7cd16009ad8ebdc6cc0446d956d2b93eeaee919d
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-08-22 21:07:54 -0300

    css profile card

--- src/components/Profile Card/ProfileCard.jsx
+++ src/components/Profile Card/ProfileCard.jsx
@@ -8,13 +8,13 @@ export function ProfileCard(){
         <div className="profile-container">
             <div className="head">
                 <div className="detalhes">
-                    <div className="img-profile">
-                        <img src="https://plus.unsplash.com/premium_photo-1682148403197-69741357d444?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Homem" />
-                    </div>
-                    <div className="text-profile">
-                        <h2>Gerson Anderson</h2>
-                        <p>ID : 2384784</p>
-                    </div>
+                    <div className="img-text">
+                        <img src="https://plus.unsplash.com/premium_photo-1682148403197-69741357d444?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Homem" className='img-profile'/>
+                        <div className="text-profile">
+                            <h2>Gerson Anderson</h2>
+                            <p>ID : 2384784</p>
+                        </div>
+                    </div>   
                     <div className="icone-profile">
                         <Bell size={32}/>
                     </div>
