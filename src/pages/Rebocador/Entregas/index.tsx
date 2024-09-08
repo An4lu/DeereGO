a189fe6c1a2608bf27b82d1b8d3514f6a435ad91
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-09-08 18:22:36 -0300

    Deletando status na tela entrega

--- src/pages/Rebocador/Entregas/index.tsx
+++ src/pages/Rebocador/Entregas/index.tsx
@@ -1,12 +1,10 @@
 import { EntregasBox } from "../../../components/EntregasBox"
 import { ProfileCard } from "../../../components/ProfileCard"
-import { Status } from "../../../components/Status"
 
 export const Entregas = () => {
     return (
         <>
             <ProfileCard/>
-            <Status/>
             <EntregasBox/>
         </>
     )
