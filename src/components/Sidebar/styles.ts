891aaf8e174b4a6c53ee84dd06339de5241302e7
AnaluPc <anabluoliveira@outlook.com>
2024-09-10 10:08:49 -0300

    Revert "Navbar completa"
    This reverts commit 761966e96ffbb0c537b27b5893fc65331bd78c0a.

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -2,29 +2,29 @@ import { NavLink } from "react-router-dom";
 import { styled } from "../../styles/global";
 
 export const SidebarContainer = styled('div', {
-    width: '300px',
+    marginLeft: '48px',
+    marginTop: '0px',
+    width: '296px',
     height: '100%',
 });
 
 export const LogoContainer = styled('div', {
+    padding: '16px',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
-    padding: '32px',
+    width: '50%',
 });
 
 export const ContentContainer = styled('div', {
     display: 'flex',
     flexDirection: 'column',
-    justifyItems: 'center',
-    alignItems: 'flex-start',
-    marginTop: '15px',
-    marginLeft: '30px',
+    alignItems: 'center',
     gap: '32px',
+    paddingTop: '16px',
 });
 
 export const SidebarItem = styled(NavLink, {
-    fontSize: '15px',
+    fontSize: '18px',
     display: 'flex',
     gap: '16px',
     alignItems: 'center',
