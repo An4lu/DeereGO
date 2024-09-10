53be2e955584f09ae6f2b64fba8e9e1799ae45c4
AnaluPc <anabluoliveira@outlook.com>
2024-09-10 10:56:08 -0300

    Teste Loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+import trator from '../../../public/logotipoverde.png'
+
+export function Loading() {
+  return (
+    <Container>
+      <img src={trator} alt="Loading" />
+    </Container>
+  )
+}
