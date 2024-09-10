3688437ceb84df1ed1f6bc2a4b4a5a72e14b2c56
AnaluPc <anabluoliveira@outlook.com>
2024-09-10 19:03:03 -0300

    correção pagina Ajustes

--- src/pages/Admin/Ajustes/index.tsx
+++ src/pages/Admin/Ajustes/index.tsx
@@ -2,7 +2,7 @@ import { Heading } from "../../../components/Heading"
 import { Background, Div } from "./styles"
 
 
-export const Carrinhos = () => {
+export const Ajustes = () => {
     return (
         <Background>
             <Heading css={{ marginBottom: '20px' }}>Ajustes</Heading>
