50b410142c36357072a5fee7330f61b9985a9f0a
AnaluPc <anabluoliveira@outlook.com>
2024-09-10 10:18:23 -0300

    Revert "error"
    This reverts commit 0454b1f0879abaafcd00ebff3c52eb5b84b83b14.

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,49 +1,39 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { LayoutAdmin } from './layouts/LayoutAdmin';
-import { LayoutRebocador } from './layouts/LayoutRebocador';
-import { DashboardAdmin } from './pages/Admin/DashboardAdmin';
-import { Login } from './pages/Login';
-import { Dashboard } from './pages/Rebocador/Dashboard';
-import { Entregas } from './pages/Rebocador/Entregas';
-import { Perfil } from './pages/Rebocador/Perfil';
-import { globalStyles } from './styles';
-import { AuthProvider } from './contexts/Auth/AuthContext'; // Certifique-se de usar o AuthProvider corretamente
-import RequireAuth from './contexts/Auth/RequireAuth'; // Certifique-se de que o RequireAuth está correto
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import { LayoutAdmin } from './layouts/LayoutAdmin'
+import { LayoutRebocador } from './layouts/LayoutRebocador'
+import { DashboardAdmin } from './pages/Admin/DashboardAdmin'
+import { Login } from './pages/Login'
+import { Dashboard } from './pages/Rebocador/Dashboard'
+import { Entregas } from './pages/Rebocador/Entregas'
+import { Perfil } from './pages/Rebocador/Perfil'
+import { globalStyles } from './styles'
 
-globalStyles();
+globalStyles()
 
 export const AppRouter = () => {
   return (
     <HashRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
+      <Routes>
+        <Route path="/" element={<Login />} />
 
-          <Route path="/" element={<Login />} />
+        <Route path="adm/*" element={
+          <LayoutAdmin>
+            <Routes>
+              <Route path='home' element={<DashboardAdmin />} />
+            </Routes>
+          </LayoutAdmin>
+        } />
 
-          <Route path="adm/*" element={
-            <RequireAuth>
-              <LayoutAdmin>
-                <Routes>
-                  <Route path="home" element={<DashboardAdmin />} />
-                </Routes>
-              </LayoutAdmin>
-            </RequireAuth>
-          } />
-
-          <Route path="rebocador/*" element={
-            <RequireAuth>
-              <LayoutRebocador>
-                <Routes>
-                  <Route path="home" element={<Dashboard />} />
-                  <Route path="entregas" element={<Entregas />} />
-                  <Route path="perfil" element={<Perfil />} />
-                </Routes>
-              </LayoutRebocador>
-            </RequireAuth>
-          } />
-        </Routes>
-      </AuthProvider>
-    </HashRouter>
-  );
-};
+        <Route path='rebocador/*' element={
+          <LayoutRebocador>
+            <Routes>
+              <Route path="home" element={<Dashboard />} />
+              <Route path="entregas" element={<Entregas />} />
+              <Route path="perfil" element={<Perfil />} />
+            </Routes>
+          </LayoutRebocador>
+        } />
+      </Routes>
+    </HashRouter >
+  )
+}
