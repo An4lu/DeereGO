f9f0f19e7b0b98299879e09a54318070ba1283eb
AnaluPc <anabluoliveira@outlook.com>
2024-09-10 21:01:01 -0300

    Fetch soma de carros-kit

--- src/pages/Admin/DashboardAdmin/index.tsx
+++ src/pages/Admin/DashboardAdmin/index.tsx
@@ -2,11 +2,27 @@ import { Heading } from "../../../components/Heading"
 import { useAuth } from "../../../contexts/AuthContext";
 import { Title } from "../../../components/Title";
 import { Background, Column01, Column02, ContainerEntregas, Div, DivContainer, Linha, Map, Row01, Row02 } from "./styles";
+import { useEffect, useState } from "react";
 
 export const DashboardAdmin = () => {
     const { user } = useAuth();
+    const [totalCarrinhos, setTotalCarrinhos] = useState<number>(0);
 
-    console.log('Usuário no Dashboard:', user);
+    useEffect(() => {
+        const fetchEntregas = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/rebocador`);
+                const data = await response.json();
+
+                const total = data.reduce((acc: number, entrega: any) => acc + entrega.TotalCarrinhos, 0);
+                setTotalCarrinhos(total);
+            } catch (error) {
+                console.error("Erro ao buscar entregas:", error);
+            }
+        };
+
+        fetchEntregas();
+    }, []);
 
     return (
         <Background>
@@ -22,7 +38,7 @@ export const DashboardAdmin = () => {
                 </Div>
                 <Div css={{ width: '80%' }}>
                     <Linha>
-                        <Row01><Title>Quantidade de Carros-Kit Entregues</Title><Title css={{ color: '$green', fontWeight: '800', fontSize: '48px', display: 'flex', justifyContent: 'flex-end' }}></Title></Row01><Row02></Row02>
+                        <Row01><Title>Quantidade de Carros-Kit Entregues</Title><Title css={{ color: '$green', fontWeight: '800', fontSize: '48px', display: 'flex', justifyContent: 'flex-end' }}> {totalCarrinhos}</Title></Row01><Row02></Row02>
                     </Linha>
                     <Linha>
                         <Map></Map>
