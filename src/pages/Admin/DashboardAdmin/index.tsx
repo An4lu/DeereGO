c4a77a923ce05badf8f59e93a75cdbdd8c1c71b9
AnaPc <anabluoliveira@outlook.com>
2024-09-20 19:25:02 -0300

    Tamanho fontes

--- src/pages/Admin/DashboardAdmin/index.tsx
+++ src/pages/Admin/DashboardAdmin/index.tsx
@@ -148,7 +148,7 @@ export const DashboardAdmin = () => {
                             <Title>
                                 Quantidade de Carros-Kit Entregues
                             </Title>
-                            <Title css={{ color: '$green', fontWeight: '800', fontSize: '48px', display: 'flex', justifyContent: 'flex-end' }}>
+                            <Title css={{ color: '$green', fontWeight: '800', fontSize: '46px', display: 'flex', justifyContent: 'flex-end' }}>
                                 {totalCarrinhos}
                             </Title>
                         </Row01>
@@ -159,13 +159,13 @@ export const DashboardAdmin = () => {
                                 </Title>
                                 <Title css={{ color: '$green', fontWeight: '800', fontSize: '35px', display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: '5px' }}>
                                     {rebocadoresAtivos.map((_, index) => (
-                                        <UserCircle key={index} size={55} weight="fill" color="#028001" />
+                                        <UserCircle key={index} size={40} weight="fill" color="#028001" />
                                     ))}
 
                                 </Title>
                             </DivRow>
                             <DivRow02>
-                                <Title css={{ color: '$green', fontWeight: '800', fontSize: '48px', }}>
+                                <Title css={{ color: '$green', fontWeight: '800', fontSize: '46px', }}>
                                     {rebocadoresAtivos.length}
                                 </Title>
                             </DivRow02>
