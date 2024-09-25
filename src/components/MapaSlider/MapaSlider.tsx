acebe2ab4f0f4f99c329dbffba31b4111c75832f
Lucas Rodrigues Grecco <lucas.rgrecco@gmail.com>
2024-09-25 08:18:43 -0300

    canvas

--- src/components/MapaSlider/MapaSlider.tsx
+++ src/components/MapaSlider/MapaSlider.tsx
@@ -1,32 +1,10 @@
-// //import images setor a
-// import SetorA from '../../assets/mapa/SETOR A.png';
-// import SetorAQ1 from '../../assets/mapa/SAQ1.png';
-// import SetorAQ2 from '../../assets/mapa/SAQ2.png';
-// import SetorAQ3 from '../../assets/mapa/SAQ3.png';
-// import SetorAQ4 from '../../assets/mapa/SAQ4.png';
-// //import images setor b
-// import SetorB from '../../assets/mapa/SETOR B.png';
-// import SetorBQ1 from '../../assets/mapa/SBQ1.png';
-// import SetorBQ2 from '../../assets/mapa/SBQ2.png';
-// import SetorBQ3 from '../../assets/mapa/SBQ3.png';
-// import SetorBQ4 from '../../assets/mapa/SBQ4.png';
-// //import images setor c
-// import SetorC from '../../assets/mapa/SETOR C.png';
-// import SetorCQ1 from '../../assets/mapa/SCQ1.png';
-// import SetorCQ2 from '../../assets/mapa/SCQ2.png';
-// import SetorCQ3 from '../../assets/mapa/SCQ3.png';
-// import SetorCQ4 from '../../assets/mapa/SCQ4.png';
-// //import images setor d
-// import SetorD from '../../assets/mapa/SETOR D.png';
-// import SetorDQ1 from '../../assets/mapa/SDQ1.png';
-// import SetorDQ2 from '../../assets/mapa/SDQ2.png';
-// import SetorDQ3 from '../../assets/mapa/SDQ3.png';
-// import SetorDQ4 from '../../assets/mapa/SDQ4.png';
+
 
 import { Canvas } from '../Canvas/Canvas';
 import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, A11y } from 'swiper/modules';
+import mapa from "/mapa-2.png"
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -34,85 +12,23 @@ import 'swiper/css/zoom';
 import 'swiper/css/scrollbar';
 import './slider.css';
 
-interface Carrinho {
-    _id: string;
-    Local: string;
-    PosX: number;
-    PosY: number;
-}
 
 interface Rebocador {
-    carrinhos: Carrinho[];
+    carrinhos: {
+        _id : string;
+        PosX: number;
+        PosY: number;
+        Local: string;
+
+    };
 }
 
 interface Dado {
     rebocadores: Rebocador[];
 }
 
-// function getImageMapa(local: string): string{
-//     console.log(local)
-//     if(local === 'SETOR A'){
-//         return SetorA;
-//     }
-//     if(local === 'A1'){
-//         return SetorAQ1;
-//     }
-//     if(local === 'A2'){
-//         return SetorAQ2;
-//     }
-//     if(local === 'A3'){
-//         return SetorAQ3;
-//     }
-//     if(local === 'A4'){
-//         return SetorAQ4;
-//     }
-//     if(local === 'SETOR B'){
-//         return SetorB;
-//     }
-//     if(local === 'B1'){
-//         return SetorBQ1;
-//     }
-//     if(local === 'B2'){
-//         return SetorBQ2;
-//     }
-//     if(local === 'B3'){
-//         return SetorBQ3;
-//     }
-//     if(local === 'B4'){
-//         return SetorBQ4;
-//     }
-//     if(local === 'SETOR C'){
-//         return SetorC;
-//     }
-//     if(local === 'C1'){
-//         return SetorCQ1;
-//     }
-//     if(local === 'C2'){
-//         return SetorCQ2;
-//     }
-//     if(local === 'C3'){
-//         return SetorCQ3;
-//     }
-//     if(local === 'C4'){
-//         return SetorCQ4;
-//     }
-//     if(local === 'SETOR D'){
-//         return SetorD;
-//     }
-//     if(local === 'D1'){
-//         return SetorDQ1;
-//     }
-//     if(local === 'D2'){
-//         return SetorDQ2;
-//     }
-//     if(local === 'D3'){
-//         return SetorDQ3;
-//     }
-//     if(local === 'D4'){
-//         return SetorDQ4;
-//     }
-//     return '';
-// }
+
+
 
 export function MapaSlider() {
     const [dados, setDados] = useState<Dado[]>([]);
@@ -139,28 +55,62 @@ export function MapaSlider() {
             pagination={{ clickable: true }}
             className='mySwiper'
         >
-            {dados.map((dado, index) => {
-                const rebocadores = Array.isArray(dado.rebocadores) ? dado.rebocadores : [];
+           {dados.flatMap((dado) => {
+                    const rebocadores = dado.rebocadores || [];
 
-                return rebocadores.map((rebocador, rebocadorIndex) => {
-                    const carrinhos = Array.isArray(rebocador.carrinhos) ? rebocador.carrinhos : [];
+                    return rebocadores.flatMap((rebocador, index) => {
+                        const carrinho = rebocador.carrinhos; // Acessa diretamente como objeto
+                        console.log('Carrinho:', carrinho);  // Verifique se os carrinhos estão corretamente atribuídos
 
-                    return carrinhos.map((carrinho) => (
-                        <SwiperSlide key={`${index}-${rebocadorIndex}-${carrinho._id}`}>
-                            <h1>{carrinho.Local}</h1>
-                            <Canvas
-                                img={carrinho.Local} 
-                                posX={carrinho.PosX}
-                                posY={carrinho.PosY}
-                                id={carrinho._id}
-                                className={`class-${carrinho._id}`}
-                                width="600"
-                                height="600"
-                            />
-                        </SwiperSlide>
-                    ));
-                });
-            })}
+                        // Verifique se o carrinho existe
+                        if (!carrinho._id) {
+                            console.warn(`Rebocador ${index} não tem carrinhos com informações válidas`);
+                            return null;
+                        }
+
+                        interface ImageMap {
+                            [key: string]: string;
+                        }
+
+                        const imageMap: ImageMap = {
+                            A: 'https://i.postimg.cc/1RMt6LG9/SETOR-A.png',
+                            A1: 'https://i.postimg.cc/t4p2fn1d/SAQ1.png',
+                            A2: 'https://i.postimg.cc/mk4HDCW8/SAQ2.png',
+                            A3: 'https://i.postimg.cc/m4JStKkb/SAQ3.png',
+                            A4: 'https://i.postimg.cc/rs9tts8c/SAQ4.png',
+                            B: 'https://i.postimg.cc/fRSTGy6L/SETOR-B.png',
+                            B1: 'https://i.postimg.cc/fTwdQcKb/SBQ1.png',
+                            B2: 'https://i.postimg.cc/J4Pkrs65/SBQ2.png',
+                            B3: 'https://i.postimg.cc/s2ZQ4wVT/SBQ3.png',
+                            B4: 'https://i.postimg.cc/Cx5fzw9T/SBQ4.png',
+                            C: 'https://i.postimg.cc/nLvVL5Vf/SETOR-C.png',
+                            C1: 'https://i.postimg.cc/gJFwS8jm/SCQ1.png',
+                            C2: 'https://i.postimg.cc/L5mJRSjW/SCQ2.png',
+                            C3: 'https://i.postimg.cc/Hn3VkN2k/SCQ3.png',
+                            C4: 'https://i.postimg.cc/v8v1h1sw/SCQ4.png',
+                            D: 'https://i.postimg.cc/4NSx2V2q/SETOR-D.png',
+                            D1: 'https://i.postimg.cc/hPyXFHvC/SDQ1.png',
+                            D2: 'https://i.postimg.cc/TPm1D19S/SDQ2.png',
+                            D3: 'https://i.postimg.cc/Hxgx4mvL/SDQ3.png',
+                            D4: 'https://i.postimg.cc/MTmG9Lgm/SDQ4.png',
+                        };
+
+                        return (
+                            <SwiperSlide key={`${index}-${carrinho._id}`}>
+                                <Canvas
+                                    posX={carrinho.PosX}
+                                    posY={carrinho.PosY}
+                                    width="400"
+                                    height="400"
+                                    style={{
+                                        backgroundImage: `url("https://png.pngtree.com/thumb_back/fh260/background/20211031/pngtree-abstract-bg-image_914283.png")`
+                                    }}
+                                />
+                            </SwiperSlide>
+                        );
+                    });
+                })}
+
         </Swiper>
     );
 }
