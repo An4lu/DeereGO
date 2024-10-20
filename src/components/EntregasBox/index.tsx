4176392ee1c742b603950fc4e5db6016f60b8229
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-10-20 13:07:12 -0300

    registros de entregas

--- src/components/EntregasBox/index.tsx
+++ src/components/EntregasBox/index.tsx
@@ -1,50 +1,74 @@
 import { CardEntrega } from '../CardEntrega';
 import { EntregasContainer } from './styles';
 import { useAuth } from '../../contexts/AuthContext';
+import { useEffect, useState } from 'react';
 
+interface Entregas {
+    _id: string;
+    IdCarrinho: string;
+    IdUser: string;
+    Partida: string;
+    Destino: string;
+    HoraPartida: string;
+    HoraEntrega: string;
+    Status: string;
+}
+interface Carro {
+    _id: string;
+    NomeCarrinho: string;
+    Local: string;
+    Peças: string;
+    Bloco: string;
+}
+
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export function EntregasBox() {
     const { user } = useAuth();
-    console.log('response user context - Entregas: ',user?.rebocadores?.[0]?.carrinhos)
+    
+    const [entregas, setEntregas] = useState<Entregas[]>([]);
+    const [carrinhos, setCarrinhos] = useState<Carro[]>([]);
 
-    user?.rebocadores?.map((rebocador) => {
-        const carrinho = rebocador.carrinhos;
-        if (carrinho?.entregas) {
-            carrinho.entregas.map((entrega: any) => {
-                console.log('response entregas - Entregas: ', entrega);
-                return (
-                    <CardEntrega
-                        key={entrega._id}
-                        idCart={entrega.IdCarrinho}
-                        titleCart={carrinho.NomeCarrinho}
-                        Partida={entrega.Partida}
-                        Destino={entrega.Destino}
-                        DataHora={entrega.HoraEntrega}
-                    />
-                )
+    useEffect(() => {
+        fetch(`${apiBaseUrl}/rebocador/entrega`)
+            .then(response => response.json())
+            .then(data => {
+                if (data) {
+                    setEntregas(data);
+                }
             });
-        }
-    })
+    }, []);
+
+    useEffect(() => {
+        fetch(`${apiBaseUrl}/rebocador/entrega/carrinho`)
+            .then(response => response.json())
+            .then(data => {
+                if (data) {
+                    setCarrinhos(data);
+                }
+            });
+    }, []);
+    
+    const getNomeCarrinho = (idCarrinho: string) => {
+        const carrinho = carrinhos.find(carrinho => carrinho._id === idCarrinho);
+        return carrinho? carrinho.NomeCarrinho : 'Carrinho';
+    };
 
 
     return (
         <EntregasContainer>
-            {user?.rebocadores?.flatMap((rebocador) => {
-                const carrinho = rebocador.carrinhos;
-                if (carrinho?.entregas) {
-                    return carrinho.entregas.map((entrega: any) => (
-                        <CardEntrega
-                            key={entrega._id}
-                            idCart={entrega.IdCarrinho}
-                            titleCart={carrinho.NomeCarrinho}
-                            Partida={entrega.Partida}
-                            Destino={entrega.Destino}
-                            DataHora={entrega.HoraEntrega}
-                        />
-                    ));
-                }
-                return [];
-            })}
+            {entregas
+                .filter(entrega => entrega.IdUser === user?.id)
+                .map(entrega => (
+                    <CardEntrega
+                        key={entrega._id}
+                        idCart={entrega.IdCarrinho}
+                        titleCart={getNomeCarrinho(entrega.IdCarrinho)}
+                        Partida={entrega.Partida}
+                        Destino={entrega.Destino}
+                        DataHora={entrega.HoraEntrega}
+                    />
+                ))}
         </EntregasContainer>
     );
 }
