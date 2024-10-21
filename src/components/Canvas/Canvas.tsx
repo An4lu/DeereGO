7f47524edcb3d858e4a9a1de5f244cce057043c3
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-10-21 16:51:36 -0300

    fix load logo mapa

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef} from "react";
+import React, { useEffect, useRef, useState} from "react";
 import { LogotipoDiv, CanvasComponent } from "./styles";
 import logo from "../../assets/shopping-cart-fill.svg";
 import { useCanvas } from "./CanvasHooks";
@@ -13,6 +13,7 @@ interface CanvasProps {
 
 export const Canvas: React.FC<CanvasProps> = ({ posX, posY, width, height, style, ...rest }) => {
     const logoImg = useRef<HTMLImageElement>(new Image());
+    const [logoLoaded, setLogoLoaded] = useState(false);
 
      // Desenhar a imagem do logo em alta resolução
      const scaleFactor = window.devicePixelRatio || 1;
@@ -21,16 +22,22 @@ export const Canvas: React.FC<CanvasProps> = ({ posX, posY, width, height, style
      const logoHeight = 38 * scaleFactor;
 
     useEffect(() => {
-        logoImg.current.src = logo;
+        const img = logoImg.current;
+        img.src = logo;
+
+        img.onload = () => {
+            setLogoLoaded(true);
+        };
     }, []);
    
     const draw = (context: CanvasRenderingContext2D, canvas: HTMLCanvasElement) => {
-        
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.beginPath();
-        context.drawImage(logoImg.current, posX*scalePx, posY*scalePx, logoWidth, logoHeight);
-        context.fill();
-        context.closePath();
+        if (logoLoaded){
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.drawImage(logoImg.current, posX*scalePx, posY*scalePx, logoWidth, logoHeight);
+            context.fill();
+            context.closePath();
+        }
     };
 
     const canvasRef = useCanvas(draw)
